b5f58c73e5bc678e4ad68db354a9ce4e1b90c9e3
Sven <68018116+SKBotNL@users.noreply.github.com>
2025-05-30 12:24:08 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.io.BufferedReader
 plugins {
     kotlin("jvm") version "2.1.21"
     id("com.gradleup.shadow") version "8.3.5"
-	id("eclipse")
+    eclipse
 }
 
 val commitHash = Runtime
