b3273d4b926f0ac24fbf7b1136cfd419f6062f33
Sven <skbotnl@disroot.org>
2026-01-27 11:33:47 +0100

    Remove bootstrap since only needed for libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "MobHeads-OG"
-
-ProcessBuilder("sh", "bootstrap.sh").directory(rootDir).inheritIO().start().let {
-    if (it.waitFor() != 0) throw GradleException("bootstrap.sh failed")
-}
